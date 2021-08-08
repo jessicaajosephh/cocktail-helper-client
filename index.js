039bb2f40f39ab86d59da700dd82445db3eaa06b
Jessica Joseph <jessicaajosephh@gmail.com>
2021-08-08 10:41:00 -0400

    defined more constants of cocktailName ingredientUl cocktailImage and cocktailInstructions

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 const cocktailsURL = "http://localhost:3000/cocktails"
-const cocktailDiv = document.getElementById("cocktail-container")
+const cocktailDiv = document.getElementById("cocktail-id")
+const cocktailName = document.getElementById("cocktail-name")
+const ingredientsUl = document.getElementById("ingredient-list")
+const cocktailImage = document.getElementById("cocktail-image")
+const cocktailInstructions = document.getElementById("instructions")
+
+
+fetchCocktails()
