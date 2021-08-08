46ae6d16caca05102bea95f11c1dc625cd6133cd
Jessica Joseph <jessicaajosephh@gmail.com>
2021-08-08 10:46:26 -0400

    created two functions; fetchCocktails and ingredientList

--- index.js
+++ index.js
@@ -5,5 +5,25 @@ const ingredientsUl = document.getElementById("ingredient-list")
 const cocktailImage = document.getElementById("cocktail-image")
 const cocktailInstructions = document.getElementById("instructions")
 
+function fetchCocktails(){
+    fetch(cocktailsURL)
+    .then(resp => resp.json())
+    .then(cocktails =>
+        cocktails.data.forEach(cocktail =>{
+            cocktailImage.src = cocktail.attributes.image 
+            cocktailName.innerText = cocktail.attributes.name 
+            ingredientList(cocktail)
+            cocktailInstructions.innerText = cocktail.attributes.instructions 
+    })
+)}
+
+function ingredientList(cocktail){
+    let ingredients = cocktail.attributes.ingredients 
+    return ingredients.map(ingredients => {
+        let li = document.createElement('li')
+        li.innerText = ingredient.name 
+        ingredientsUl.appendChild(li)
+    })
+}
 
 fetchCocktails()
